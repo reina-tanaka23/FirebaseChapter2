48f93b1371ee3fe007b618c9277acdc7aa09f598
reina.tanaka <ponzusoup.223@gmail.com>
2021-09-26 23:13:09 +0900

    Initial Commit

---
+++ FirebaseChapter2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FirebaseChapter2
+//
+//  Created by reina.tanaka on 2021/09/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
