757b9426e26ef42f31eecd091c6fe9cf21421209
reina.tanaka <ponzusoup.223@gmail.com>
2021-12-19 21:13:15 +0900

    保存処理を追加

--- FirebaseChapter2/ViewController.swift
+++ FirebaseChapter2/ViewController.swift
@@ -6,14 +6,48 @@
 //
 
 import UIKit
+import Firebase
+import GoogleSignIn
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, GIDSignInDelegate, GIDSignInUIDelegate {
 
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var pass: UITextField!
+    @IBOutlet weak var signInButton: GIDSignInButton!
+    
+    var changed: AuthStateDidChangeListenerHandle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        label.text = "email & pass."
+        pass.isSecureTextEntry = true
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        changed = Auth.auth().addStateDidChangeListener { (auth, user) in
+            self.label.text = user?.email
+        }
     }
 
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        Auth.auth().removeStateDidChangeListener(changed)
+    }
 
+    @IBAction func doAction(_ sender: Any) {
+        let email = self.email.text
+        let password = self.pass.text
+        Auth.auth().signIn(withEmail: email ?? "", password: password ?? "") { (user, error) in
+            if error != nil {
+                self.label.text = error?.localizedDescription
+                return
+            }
+        }
+    }
+    
+    func sign(_ signIn: GIDSignIn!, didSignInFor user: GIDGoogleUser!, withError error: Error!) {
+        
+    }
 }
 
---
+++ Podfile
@@ -0,0 +1,13 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '14.5'
+
+target 'FirebaseChapter2' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for FirebaseChapter2
+  pod 'Firebase/Core'
+  pod 'Firebase/Auth'
+  pod 'GoogleSignIn','< 5.0.0'
+
+end
